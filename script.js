4f640568227b8760fb1a7d092694afbab19b84c6
jumathhh <mathju2024@gmail.com>
2024-10-30 08:54:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//spread operator
+let primeiros = [1, 2, 3];
+
+let numeros = [primeiros,4,5,6];
+
+let numeros2 = [...primeiros, 4, 5, 6];
+
+console.log(numeros);
+console.log(numeros2);
+
+//spredando num objeto
+let pessoa = {
+    nome: "Julia",
+    idade : 16,
+    cargo: "estudante"
+};
+
+let dadosPessoa = {
+    ...pessoa,
+    status: "Ativo",
+    cidade: "Campo Largo",
+};
+
+console.log(dadosPessoa);
+console.log(dadosPessoa.cargo);
+
+//ecemplificando de uma forma mais realista
+
+function novoUsuario(info) {
+    //console.log(info);
+    let dados =  {
+        ...info,
+        status: 'ativo',
+        inicio: '20/09/2024',
+        codigo: '123'
+    };
+    console.log(dados);
+}
+
+novoUsuario({nome: "Admilson", sobreNome: "Rico", cargo: "estagiário"});
+
